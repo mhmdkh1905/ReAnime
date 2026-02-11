1f4952a262b14c7709c5f3303a1e5b732f781c0f
khalilhamody9 <khalilhamody69@gmail.com>
2026-02-11 19:26:51 +0200

    fix router and layout

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 export default function RootLayout() {
   return (
     <>
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,27 +1,24 @@
-import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import RootLayout from "../components/layout/RootLayout";
-
 import Home from "../pages/Home";
 import Movie from "../pages/Movie";
 import Profile from "../pages/Profile";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
-export default function router() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: "movie", element: <Movie /> },
-        { path: "profile", element: <Profile /> },
-        { path: "login", element: <Login /> },
-        { path: "register", element: <Register /> },
-        // { path: "*", element: <PageNotFound /> },
-      ],
-    },
-  ]);
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "movie", element: <Movie /> },
+      { path: "profile", element: <Profile /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+    ],
+  },
+]);
+
+export default router;
