f042a866a3f8c8cf2d67448aa3f64af6bc6f779b
mhmdkh1905 <mhmd52kh@gmail.com>
2026-02-13 00:36:12 +0200

    navbar + movieCard (frontend)

--- src/components/movie/MovieCard.jsx
+++ src/components/movie/MovieCard.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import "../../styles/MovieCard.css";
 
-export default function MovieCard() {
+export default function MovieCard({ title, image, type, trending }) {
   return (
-    <div>MovieCard</div>
-  )
+    <div className="movie-card">
+      <div className="movie-image">
+        <img src={image} alt={title} />
+        <div className="overlay">
+          <div className="movie-info">
+            <h2>{title}</h2>
+
+            <div className="movie-meta">
+              <span className="badge">{type}</span>
+            </div>
+            {trending === "true" && (
+              <div className="trending">
+                <span className="dot"></span>
+                Trending
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import "../styles/home.css";
+import MovieCard from "../components/movie/MovieCard.jsx";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="content">
+        <p className="title">Reimagine Your Favorite Stories</p>
+        <p className="description">
+          Explore anime worlds, watch iconic scenes, and rewrite the narrative
+          as the character or creator.
+        </p>
+      </div>
+      <div className="MoviesGrid">
+        <MovieCard
+          title="Neon Dreams"
+          image="https://img.freepik.com/premium-photo/flying-popcorn-3d-glasses-film-reel-clapboard-yellow-background-cinema-movie-concept-3d_989822-1302.jpg?semt=ais_wordcount_boost&w=740&q=80"
+          type="Action"
+          trending="true"
+        />
+      </div>
+    </div>
+  );
 }
